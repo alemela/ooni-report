096c25bddcdc7338c9fcc6fd953c4722d8fa8ab5
DavideAllavena <davide.allavena@gmail.com>
2015-10-01 19:09:03 +0200

    Partial dns_injection report implemented. Not Fully tested :banana:

--- report.js
+++ report.js
@@ -38,19 +38,60 @@ var tcp_connectDigest = function (data) {
     }
 }
 
+var dns_injectionDigest = function (data) {
+    if (data.record_type === "entry" && data.connection !== undefined) {
+        var json = {};
+        var date = yamlator.calcDate(data.start_time);
+        var outputFile = 'output/' + data.probe_cc + "_" + date.year + "_" + date.month + ".json";
+        if (fs.existsSync(outputFile)) {
+            json = JSON.parse(fs.readFileSync(outputFile));
+        }
+        if (json.dnsInjection === undefined) {
+            json.dnsInjection = {};
+            json.dnsInjection.versions = [];
+            json.dnsInjection.domains = [];
+        }
+        json.dnsInjection.versions.push(data.test_version);
+
+        var flag = 0;
+        for (var i = 0; i < json.dnsInjction.domains.length; i++) {
+            if (json.dnsInjection.domains[i].url === data.input) {
+                json.dnsInjection.domains[i].totalTests++;
+                if (data.connection === "success")
+                    json.dnsInjection.domains[i].totalSucceded++;
+                flag = 1;
+                break;
+            }
+        }
+
+        if (!flag) {
+            var obj = {};
+            obj.url = data.input;
+            obj.totalTests = 1;
+            obj.totalSucceded = 1;
+            json.dnsInjection.domains.push(obj);
+        }
+        fs.writeFileSync(outputFile, JSON.stringify(json, null, 4));
+    }
+}
+
 yamlator.searchDir(process.argv[2], function (d) {
     d.forEach(function (dir) {
-        yamlator.searchDir(process.argv[2]+"/"+dir, function (f) {
-            f.forEach(function (file) {
-                if (file.indexOf('tcp_connect') > -1) {
-                    yamlator.digestYamlArchived(process.argv[2]+dir+"/"+file, function (d) {
-                        tcp_connectDigest(d);
-                    });
-                }
-                if (file.indexOf('dns_injection') > -1) {
-                    //yamlator.digestYamlArchived(process.argv[2]+dir+"/"+file);
-                }
+      if (fs.lstatSync(process.argv[2] + "/" + dir).isDirectory()) {
+            yamlator.searchDir(process.argv[2]+"/"+dir, function (f) {
+                f.forEach(function (file) {
+                    if (file.indexOf('tcp_connect') > -1) {
+                        yamlator.digestYamlArchived(process.argv[2]+dir+"/"+file, function (d) {
+                            tcp_connectDigest(d);
+                        });
+                    }
+                    if (file.indexOf('dns_injection') > -1) {
+                        yamlator.digestYamlArchived(process.argv[2]+dir+"/"+file, function (d) {
+                           dns_injectionDigest(d);
+                       });
+                    }
+                });
             });
-        });
+        }
     });
 });
