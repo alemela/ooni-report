adfe5b2ff7f9fc087975e148055f0266d4feb04e
DavideAllavena <davide.allavena@gmail.com>
2015-10-02 17:22:26 +0200

    Added test (http_header_field_manipulation). Small fix.

--- tests.js
+++ tests.js
@@ -112,7 +112,7 @@ var dns_injectionDigest = function (data) {
             if (json.dnsInjection.domains[i].url === data.input) {
                 json.dnsInjection.domains[i].totalTests++;
                 if (data.injected === false)
-                    json.dnsInjection.domains[i].totalNonInjected++;
+                    json.dnsInjection.domains[i].totalNotInjected++;
                 flag = 1;
                 break;
             }
@@ -123,9 +123,9 @@ var dns_injectionDigest = function (data) {
             obj.url = data.input;
             obj.totalTests = 1;
             if (data.injected === false) {
-               obj.totalNonInjected = 0;
+               obj.totalNotInjected = 0;
             } else {
-               obj.totalNonInjected = 1; 
+               obj.totalNotInjected = 1; 
             }
             json.dnsInjection.domains.push(obj);
         }
@@ -171,7 +171,7 @@ var http_invalid_request_lineDigest = function (data) {
         for (var i = 0; i < json.httpInvalidRequestLine.domains.length; i++) {
             if (json.httpInvalidRequestLine.domains[i].url === data.input) {
                 json.httpInvalidRequestLine.domains[i].totalTests++;
-                if (data.tampered === true)
+                if (data.tampering === true)
                     json.httpInvalidRequestLine.domains[i].totalNotTamperd++;
                 flag = 1;
                 break;
@@ -182,7 +182,7 @@ var http_invalid_request_lineDigest = function (data) {
             var obj = {};
             obj.url = data.input;
             obj.totalTests = 1;
-            if (data.tampered === false) {
+            if (data.tampering === false) {
             obj.totalNotTamperedSucceded = 1;
             } else {
             obj.totalNotTampered = 0;
@@ -196,6 +196,107 @@ var http_invalid_request_lineDigest = function (data) {
         });
     }
 }
+
+var http_header_field_manipulationDigest = function (data) {
+    if (data.record_type === "entry" && data.connection !== undefined) {
+        var json = {};
+        var kFlag = 1;
+        var date = yamlator.calcDate(data.start_time);
+        var id = data.probe_cc + "_" + date.year + "_" + date.month;
+        
+        mainArray.forEach(function (e) {
+            if (e.key === id) {
+                json = e.json;
+                kFlag = 0;
+            }
+        });
+
+        if (kFlag) {
+            var o = {}
+            o.key = id;
+            o.json = {};
+            mainArray.push(o);
+        }
+
+        if (json.httpHeaderFieldManipulation === undefined) {
+            json.httpHeaderFieldManipulation = {};
+            json.httpHeaderFieldManipulation.versions = [];
+            json.httpHeaderFieldManipulation.domains = [];
+        }
+        if (!indexOf(json.httpHeaderFieldManipulation.versions, data.test_version))
+            json.httpHeaderFieldManipulation.versions.push(data.test_version);
+
+        var flag = 0;
+        for (var i = 0; i < json.httpHeaderFieldManipulation.domains.length; i++) {
+            if (json.httpHeaderFieldManipulation.domains[i].url === data.input) {
+                json.httpHeaderFieldManipulation.domains[i].totalTests++;
+                if (data.tampering.header_field_name === true)
+                    json.httpHeaderFieldManipulation.domains[i].header_field_name++;
+                if (data.tampering.header_field_number === true)
+                    json.httpHeaderFieldManipulation.domains[i].header_field_number++;
+                if (data.tampering.header_field_value  === true)
+                    json.httpHeaderFieldManipulation.domains[i].header_field_value++;
+                if (data.tampering.header_name_capitalization  == true)
+                    json.httpHeaderFieldManipulation.domains[i].header_name_capitaliazation++;
+                if (data.tampering.header_name_diff  == true)
+                    json.httpHeaderFieldManipulation.domains[i].header_name_diff++;
+ 
+                flag = 1;
+                break;
+            }
+        }
+
+        if (!flag) {
+            var obj = {};
+            obj.url = data.input;
+            obj.totalTests = 1;
+            if (data.tampering.header_field_name === true) {
+               json.httpHeaderFieldManipulation.domains[i].header_field_name = 1;
+               console.log(1);
+            } else {
+               json.httpHeaderFieldManipulation.domains[i].header_field_name = 0;
+           
+               console.log(1);
+            } 
+            if (data.tampering.header_field_number === true) {
+                console.log(1);
+              json.httpHeaderFieldManipulation.domains[i].header_field_number = 1;
+            } else {
+                 console.log(1);
+             json.httpHeaderFieldManipulation.domains[i].header_field_number = 0;
+            }
+            if (data.tampering.header_field_value  === true) {
+                  console.log(1);
+            json.httpHeaderFieldManipulation.domains[i].header_field_value = 1;
+            } else {
+                   console.log(1);
+           json.httpHeaderFieldManipulation.domains[i].header_field_value = 0;
+            }
+            if (data.tampering.header_name_capitalization  == true) {
+                    console.log(1);
+          json.httpHeaderFieldManipulation.domains[i].header_name_capitaliazation = 1;
+            } else {
+                     console.log(1);
+         json.httpHeaderFieldManipulation.domains[i].header_name_capitaliazation = 0;
+            }
+            if (data.tampering.header_name_diff  == true) {
+                      console.log(1);
+        json.httpHeaderFieldManipulation.domains[i].header_name_diff = 1;
+            } else {
+                       console.log(1);
+       json.httpHeaderFieldManipulation.domains[i].header_name_diff = 0;
+            }
+            json.httpHeaderFieldManipulation.domains.push(obj);
+        }
+
+        mainArray.forEach(function (e) {
+            if (e.key === id)
+                e.json = json;
+        });
+    }
+}
+
+
 yamlator.searchDir(process.argv[2], function (d) {
     d.forEach(function (dir) {
       if (fs.lstatSync(process.argv[2] + "/" + dir).isDirectory()) {
